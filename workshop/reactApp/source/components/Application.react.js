2695ece0c0439b81c9d458f7b48eac6d238f7c2e
TheVoilaApp <jose.canahui@gmail.com>
2015-10-29 23:53:42 -0400

    added react library and set output to cordova www folder

---
+++ workshop/reactApp/source/components/Application.react.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Application = React.createClass({ 
+
+	render: function () {
+		return (
+			<div className="container-fluid">
+				<p>This is the react app!</p>
+			</div>
+		);
+	}
+ });
+
+module.exports = Application;
