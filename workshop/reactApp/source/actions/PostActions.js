fc90a04a63d3efac48805ae13fb6e120af673bb1
TheVoilaApp <jose.canahui@gmail.com>
2015-12-17 07:20:34 -0500

    made the urls correct

--- workshop/reactApp/source/actions/PostActions.js
+++ workshop/reactApp/source/actions/PostActions.js
@@ -4,7 +4,7 @@ import request from 'reqwest';
 export default {
   savePost: function(name, fields, position) {
     request({
-      url: '45.55.182.4/voz/post',
+      url: 'http://45.55.182.4/voz/post',
       method: 'POST',
       crossOrigin: true,
       data: {
@@ -21,7 +21,7 @@ export default {
   },
   getPosts: function() {
     request({
-      url: '45.55.182.4/voz/post',
+      url: 'http://45.55.182.4/voz/post',
       method: 'GET',
       crossOrigin: true
     }).then((res) => {
